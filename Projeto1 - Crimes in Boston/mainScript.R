ad65eb912d0db8af6d7848680e2ccb711ed03e05
Ventura <gustavo.ventura100@gmail.com>
2020-09-24 07:31:55 -0300

    Adição exploração inicial dataset crimes

--- Projeto1 - Crimes in Boston/mainScript.R
+++ Projeto1 - Crimes in Boston/mainScript.R
@@ -52,3 +52,62 @@ ggplot(convertedDistrict ,aes(x = distritos, y = Freq)) +
 #Quando os crimes ocorrreram? - Para o crime de assassinato, drogas(drug violation)
 #e furto (larceny),  plote um gráfico que apresente índices de acordo com os horários do dia (independente do ano)
 
+##################### PARTE VENTURA ###########################
+
+library("dplyr")
+library("ggplot2")
+library("tidyr")
+
+# setwd("~/Documents/R aulas/projeto1")
+
+# ------------------------------------ Leitura do CSV
+crimes <- read.csv("crime.csv", sep=",", na.strings = c('','NA','na','N/A','n/a','NaN','nan'))
+crimes
+
+# ----------------------------------- Exploração do dataset
+nrow(crimes)
+ncol(crimes)
+dim(crimes)
+
+View(head(crimes, n=5))
+str(crimes)
+
+# Após a visalização de uma amostra do dataset com 10 linhas 
+# foi possivel perceber valores vazios para coluna SHOOTING, 
+# então foi realizado um exploração para entender o numero de dados vazios nas features.
+# Desta forma SHOTTING apresentou o maior numero de dados vazios com 318054 registros NA.
+
+View(sapply(crimes, function(x) sum(is.na(x))))
+
+# Retorna a menor e maior data de ocorrencia de crime presente no dataset
+min(crimes$OCCURRED_ON_DATE)
+max(crimes$OCCURRED_ON_DATE)
+
+
+# ------------------- Exploração inicial com medidas estatísticas
+head(crimes$YEAR, 10)
+tail(crimes$YEAR, 10)
+data_2015_2010<- filter(crimes, YEAR%in%c(2015, 2010))
+
+
+# Retornar o menor e o maior indice de latitude e longitude para realizar a escala do mapa de crimes de boston
+maior_lat = max(crimes$Lat, na.rm=T)
+menor_lat = min(crimes$Lat, na.rm=T)
+
+maior_long = max(crimes$Long, na.rm=T)
+menor_long = min(crimes$Long, na.rm=T)
+
+
+# Mapa dos crimes agrupado por distrito de Boston
+ggplot(crimes, aes(x = Long, y = Lat, group=DISTRICT, colour = DISTRICT))+
+  geom_point(size = 1, alpha = 1) + 
+  xlim(-71.15,-70.90) +
+  ylim(42.2,42.4) +
+  ggtitle("Crimes ocorridos na região de Boston separado por distritos")
+
+# Mapa dos crimes agrupados por tipo de ofensa
+ggplot(crimes, aes(x = Long, y = Lat, group=OFFENSE_CODE, colour = OFFENSE_CODE))+
+  geom_point(size = 1, alpha = 1) + 
+  xlim(-71.15,-70.90) +
+  ylim(42.2,42.4) +
+  ggtitle("Crimes ocorridos na região de Boston separado por tipos ofensa de crimes")
