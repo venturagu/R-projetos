b62765c89aeff32220ae7c69040d9aed27d0fc3a
Rivardo Vieira <ricardo.biscola.vieira@gmail.com>
2020-09-25 20:45:47 -0300

    Update na exploração estatistica

--- Projeto1 - Crimes in Boston/mainScript.R
+++ Projeto1 - Crimes in Boston/mainScript.R
@@ -36,6 +36,9 @@ crimes_mais_cometidos
 ggplot(crimes_mais_cometidos ,aes(x = categorias_de_crimes, y = Freq)) +
   geom_point()
 
+#Histogramas de tipos de ocorrencia
+barplot(crimes_mais_cometidos$Freq, names.arg = crimes_mais_cometidos$categorias_de_crimes)
+
 #Distritos com mais ocorrencias 
 distritos <- factor(crimes$DISTRICT)
 distritosTable <- table(distritos)
@@ -44,11 +47,18 @@ convertedDistrict
 
 ggplot(convertedDistrict ,aes(x = distritos, y = Freq)) +
   geom_point()
+##animals <- c("cat", "dog",  "dog", "dog", "dog", "dog", "dog", "dog", "cat", "cat", "bird")
+#ggplot(data.frame(animals), aes(x=animals)) +
+#  geom_bar()
+ggplot(data.frame(crimes$OFFENSE_CODE), aes(x=Tes)) +
+  geom_bar()
 
-
+barplot(convertedDistrict$Freq, names.arg = convertedDistrict$distritos)
 
 #Fim das medidas estatisticas
 
 #Quando os crimes ocorrreram? - Para o crime de assassinato, drogas(drug violation)
 #e furto (larceny),  plote um gráfico que apresente índices de acordo com os horários do dia (independente do ano)
-
+crimes_filter <- filter(crimes, OFFENSE_CODE_GROUP == "Drug Violation" 
+                        | OFFENSE_CODE_GROUP == "Larceny" | OFFENSE_CODE_GROUP == "Murder")
+nrow(crimes_filter) 
