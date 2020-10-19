10f9c0fa225333061593bfaf66a457693bcf2bdb
Ventura <gustavo.ventura100@gmail.com>
2020-10-19 13:45:55 -0300

    Dataset de treino e teste

--- IA/Projeto 1 - Breast Cancer/main.R
+++ IA/Projeto 1 - Breast Cancer/main.R
@@ -6,14 +6,14 @@ library(reshape2)
 
 data <- read.csv2("wdbc.data", sep =",", na.strings = c('','NA','na','N/A','n/a','NaN','nan'), header = FALSE)
 
-#Renomeando as colunas com base no indicado no link do dataset https://www.kaggle.com/uciml/breast-cancer-wisconsin-data
+# Renomeando as colunas com base no indicado no link do dataset https://www.kaggle.com/uciml/breast-cancer-wisconsin-data
 cancers <- data %>%
   rename(
     id = V1,
     diagnosis = V2, # Significado: Câncer de mama M -Maligno, B - Beligno
     radius_mean = V3, # Significado: Raio medio do câncer
     texture_mean = V4, # Significado: Média de escala de cinza do câncer
-    perimeter_mean = V5,# Significado: Media de perimetro do câncer
+    perimeter_mean = V5, # Significado: Media de perimetro do câncer
     area_mean = V6, # Significado: Média de area do câncer 
     smoothness_mean = V7,
     compactness_mean = V8,
@@ -44,3 +44,10 @@ cancers <- data %>%
   )
 
 
+# Dataset de treino 75%  da base original e teste 25% base original.
+set.seed(123)
+smp_size <- floor(0.75 * nrow(cancers))
+train_ind <- sample(seq_len(nrow(cancers)), size = smp_size)
+
+train <- cancers[train_ind, ]
+test <- cancers[-train_ind, ]
