15cd0e26bca825778e1fb8aa7d493170b87b31ba
Ventura <gustavo.ventura100@gmail.com>
2020-10-29 10:10:30 -0300

    Dataframe com o conteúdo de todos os arquivos

--- BD/Projeto 2 - Digitos/mainScript.R
+++ BD/Projeto 2 - Digitos/mainScript.R
@@ -4,33 +4,62 @@ library(ggplot2)
 library(dslabs)
 library(reshape2) # melt a data.frame
 
-# ------------------------------------ Leitura do CSV --------------------------
+# ------------------------------------ First task -----------------------------
+# Leitura de um arquivo da quarta linha em diante.
 file <- read.csv("./DigitosCompleto/0_001.BMP.inv.pgm", header = FALSE, skip = 3, sep = " ")
-files <- list.files(path = "./DigitosCompleto")
-file$V18 <- NULL
 
-# ----------------------------------- Exploração do arquivo --------------------
-nrow(file)
-ncol(file)
-dim(files)
+# Removendo ultima coluna que contém apenas dados vazios
+file <- file[-18]
 
-View(head(files, n=5))
-str(files)
+# Exploração do arquivo
+dim(file)
+View(head(file, n=5))
+str(file)
 
-# Tranformar em uma matrix 64x64
+# Transformar em uma matrix 64x64
 v<-as.vector(t(file))
+NROW(v) # Temos 4097 linhas
+NCOL(v) # Temos 1 coluna
 v<-v[-4097]
 v<-as.numeric(v)
+v <- t(v)
 mt<-matrix(v, byrow =F, 64,64)
 
-#Identificar o zero 
+# Apresentação do conteúdo do arquivo
+image(1:64, 1:64, mt, col=gray((0:255)/255))
 
-#Gerar um dataframe a partir dos arquivos lidos ---------------------
+# ------------------------------------ Second task -----------------------------
+# Montar um dataframe com o conteúdo de todos os arquivos
+
+# Caminho raiz do CSV
+files <- list.files(path = "./DigitosCompleto")
+
+# Identificar o zero 
+
+#Gerar um dataframe a partir dos arquivos lidos
 #Cada arquivo sera uma linha do dataframe
-vect_files <-as.vector(t(files))
-main_table
+
+# Vetor com nome de todos os arquivos
+vect_files <- as.vector(t(files))
+
+# Ambiente de Teste - 4 arquivos para leitura : (descomentar)
+# vect_files <- head(vect_files,4)
+
+df <- data.frame()
 
 for (x in vect_files) {
-  file_x <- read.csv("./DigitosCompleto/"+x, header = FALSE, skip = 3, sep = " ")
-  v<-as.vector(t(file_x))
+  filepath <- file.path(paste("./DigitosCompleto/", x ,sep=""))
+  file_read <- read.csv(filepath, header = FALSE, skip = 3, sep = " ")
+  file_read <- file_read[-18]
+  v<-as.vector(t(file_read))
+  v<-v[-4097]
+  v<-as.numeric(v)
+  v <- t(v)
+  print(length(v))
+  file_name <- unlist(strsplit(x, "\\."))
+  file_name <- unlist(strsplit(file_name[1], "\\_"))
+  v <- cbind(number = file_name[1], v)
+  df <- rbind(df, v)
 }
+
+View(df)
