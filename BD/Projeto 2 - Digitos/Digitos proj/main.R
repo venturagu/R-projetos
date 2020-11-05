45d59d37481d27218526937f837da22fd11a1009
Rivardo Vieira <ricardo.biscola.vieira@gmail.com>
2020-11-05 12:03:44 -0300

    Nova estrutura projeto de digitos

--- BD/Projeto 2 - Digitos/Digitos proj/main.R
+++ BD/Projeto 2 - Digitos/Digitos proj/main.R
@@ -7,21 +7,29 @@ library(stringr)
 library(class)
 library(rpart)
 library(rpart.plot)
+library('caret')
+library(plyr)
 install.packages('caret')
 
-data <- read.csv2("wdbc.data", sep =",", na.strings = c('','NA','na','N/A','n/a','NaN','nan'), header = FALSE,  dec=".", stringsAsFactors=FALSE)
+
 
 # ------------------------------------ First task -----------------------------
 # Leitura de um arquivo da quarta linha em diante.
 file <- read.csv("./DigitosCompleto/0_001.BMP.inv.pgm", header = FALSE, skip = 3, sep = " ")
 
+
+
 # Removendo ultima coluna que contém apenas dados vazios
 file <- file[-18]
+
+
 
 # Exploração do arquivo
 dim(file)
 View(head(file, n=5))
 str(file)
+
+
 
 # Transformar em uma matrix 64x64
 v<-as.vector(t(file))
@@ -32,27 +40,45 @@ v<-as.numeric(v)
 v <- t(v)
 mt<-matrix(v, byrow =F, 64,64)
 
+
+
 # Apresentação do conteúdo do arquivo
 image(1:64, 1:64, mt, col=gray((0:255)/255))
+
+
 
 # ------------------------------------ Second task -----------------------------
 # Montar um dataframe com o conteúdo de todos os arquivos
 
+
+
 # Caminho raiz do CSV
 files <- list.files(path = "./DigitosCompleto")
 
+
+
 # Identificar o zero 
+
+
 
 #Gerar um dataframe a partir dos arquivos lidos
 #Cada arquivo sera uma linha do dataframe
 
+
+
 # Vetor com nome de todos os arquivos
 vect_files <- as.vector(t(files))
 
-# Ambiente de Teste - 4 arquivos para leitura : (descomentar)
-# vect_files <- head(vect_files,4)
+
+
+# Ambiente de Teste - 34 arquivos para leitura : (descomentar)
+# vect_files <- head(vect_files,35)
+
+
 
 df <- data.frame()
+
+
 
 for (x in vect_files) {
   filepath <- file.path(paste("./DigitosCompleto/", x ,sep=""))
@@ -62,12 +88,17 @@ for (x in vect_files) {
   v<-v[-4097]
   v<-as.numeric(v)
   v <- t(v)
-  print(length(v))
+  
   file_name <- unlist(strsplit(x, "\\."))
   file_name <- unlist(strsplit(file_name[1], "\\_"))
   v <- cbind(number = file_name[1], v)
-  v <- as.data.frame(v)
-  df <- rbind.fill(df, v)
+  v <- as.numeric(v)
+  
+  if(NCOL(v) != 4096){
+    df <- rbind(df, v)  
+  }
 }
+
+
 
 View(df)
